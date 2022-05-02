404fe430fefc0df8e7b41fdada9dd235ebf5c1b3
amy <22689376@student.uwa.edu.au>
2022-05-02 16:41:44 +1000

    Added registration page/form

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
 import os
 from flask import render_template, flash, redirect
 from flask_login import current_user, login_user, logout_user
-from app import app
-from app import word_gen
+from app import app, word_gen, db
 from datetime import date
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 import json
 
 
@@ -59,3 +58,17 @@ def dailyWord():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('game'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
