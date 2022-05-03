ac68bae854cdb743b7e496bb0cc9c9f82b0d9cee
amy <22689376@student.uwa.edu.au>
2022-05-03 13:17:51 +1000

    Add 'GET' and 'POST' methods

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 import os
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 from app import app, word_gen, db
+from app.models import User
 from datetime import date
 from app.forms import LoginForm, RegistrationForm
 import json
@@ -17,29 +18,29 @@ def index():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('game'))        
+        return redirect(url_for('game'))     
     return render_template('index.html', title='Login - Anagram-City', form=form)
 
 
-@app.route('/contacts')
+@app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
     return render_template('contacts.html', title='Contacts Page')
 
 
-@app.route('/game')
+@app.route('/game', methods=['GET', 'POST'])
 def game():
     return render_template('game.html', title="Anagram-City", game=True)
 
 
-@app.route('/stats')
+@app.route('/stats', methods=['GET', 'POST'])
 def stats():
     return render_template('stats.html', title="Leaderboards")
 
 
 PUZZLE_DIR = os.getcwd()+'/app/static/dailyPuzzles/'
-@app.route('/anagram', methods=['GET'])
+@app.route('/anagram', methods=['GET', 'POST'])
 def dailyWord():
     current_date = date.today().strftime("%d-%m-%Y")
 
@@ -54,7 +55,7 @@ def dailyWord():
         data = json.load(f)
         return data
 
-@app.route('/logout')
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
     return redirect(url_for('index'))
@@ -71,4 +72,4 @@ def register():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register - Anagram City', form=form)
