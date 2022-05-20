63190e63403c9841c8b66f3f293c593d10f05220
Tom <1991tnguyen@gmail.com>
2022-05-20 09:15:07 +0800

    fixed config

--- config.py
+++ config.py
@@ -2,15 +2,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Anagram-City-B***'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'Anagram-City-B***'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# NOT IMPLEMENTED
-# class ProductionConfig(Config):
-#   ENV='production'
-#   SECRET_KEY = os.environ['SECRET_KEY']
-#   SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] 
+class ProductionConfig(Config):
+  ENV='production'
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'Anagram-City-B***'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class DevelopmentConfig(Config):
   FLASK_ENV='development'
